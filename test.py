2aa4ac4d9bb402bbe3a14b98abdccd05c592b73a
Micperry <michaelargel@gmail.com>
2020-06-02 23:02:34 +0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+#!D:/Users/michaela/AppData/Local/Programs/Python/Python38-32/python.exe
+
+x = "hello world"
+print ("Content-type: text/html\n\n")
+print (x)
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
